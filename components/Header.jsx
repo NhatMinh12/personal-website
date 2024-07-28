3a200dab8c342cda411022c279d417646fc1966c
Ken Pham <phammin5@msu.edu>
2024-07-28 15:37:20 +0700

    fix scroll loop issue

--- components/Header.jsx
+++ components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
       className={`${
         header
           ? "py-4 bg-white shadow-lg dark:bg-accent"
-          : "py-6 dark:bg-transparent"
+          : "py-4 dark:bg-transparent"
       } sticky top-0 z-30 transition-all ${pathname === '/' && 'bg-[#fef9f5]'}`}
     >
       <div className="container mx-auto">
