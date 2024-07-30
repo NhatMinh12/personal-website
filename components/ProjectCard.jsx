a11aba31849497dfd0f97e1c823051054e3735d9
Ken Pham <phammin5@msu.edu>
2024-07-30 18:08:03 +0700

    minor changes in design

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -8,18 +8,26 @@ const ProjectCard = ({ project }) => {
     <Card className="flex items-center gap-x-4 border-blue-400">
       <div className="h-full px-8 py-6">
         <Badge className="uppercase text-sm font-medium mb-2 top-4 left-5">
-            {project.category}
+          {project.category}
         </Badge>
-        <h4 className="h4 mb-1">{project.name}</h4>
+        <div className="flex gap-x-8 justify-around">
+          <h4 className="h4 mb-1">{project.name}</h4>
+          <div className="flex gap-x-8">
+            <Link href={project.link}>
+              <RiLink
+                className="hover:text-primary transition-all text-foreground"
+                size={28}
+              />
+            </Link>
+            <Link href={project.github}>
+              <RiGithubLine
+                className="hover:text-primary transition-all text-foreground"
+                size={28}
+              />
+            </Link>
+          </div>
+        </div>
         <p className="text-muted-foreground text-lg">{project.description}</p>
-      </div>
-      <div className="flex gap-x-8">
-        <Link href={project.link}>
-            <RiLink className="hover:text-primary transition-all text-foreground" size={28}/>
-        </Link>
-        <Link href={project.github}>
-            <RiGithubLine className="hover:text-primary transition-all text-foreground" size={28}/>
-        </Link>
       </div>
     </Card>
   );
--- components/ThemeToggler.jsx
+++ components/ThemeToggler.jsx
@@ -13,7 +13,7 @@ const ThemeToggler = () => {
         size="icon"
         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       >
-        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <SunIcon className="h-[2.2rem] xl:h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       </Button>
     </div>
